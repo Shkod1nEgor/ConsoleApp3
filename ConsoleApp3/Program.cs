598e478fd459e6a4d7e297bf74810aac062dc0bb
wsr12 <wsr12@K218PC3>
2022-04-11 13:56:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double b = 11;
+            var c = 2;
+            Math.Sqrt(a);
+            Console.WriteLine(12 % 4);
+            Console.WriteLine(a + " "+b+ " " +c);
+
+        }
+    }
+}
